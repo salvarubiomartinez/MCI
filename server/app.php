7cac28f31e334d80916f4ddc5373a07791dd17e6
BETWEEN\srubio <salvador.rubio@between.tech>
2017-04-06 15:37:36 +0200

    login

--- server/app.php
+++ server/app.php
@@ -10,7 +10,7 @@ require './server/vendor/autoload.php';
 
 $mysqli = new mysqli('127.0.0.1', 'mci', 'mci', 'mci');
 
-$users = array(array('email' => 'pepe@yahoo.es', 'psswd' => '$2y$10$.4O1NR9FSOztr52bLw7aC.inKjVi0uLZ6SSTSYLELuAJmcerF4B7W'),array('email' => 'juan@gmail.com', 'psswd' => '$2y$10$.4O1NR9FSOztr52bLw7aC.inKjVi0uLZ6SSTSYLELuAJmcerF4B7W'),array('email' => 'maria@yahoo.es', 'psswd' => '$2y$10$.4O1NR9FSOztr52bLw7aC.inKjVi0uLZ6SSTSYLELuAJmcerF4B7W'));
+//$users = array(array('email' => 'pepe@yahoo.es', 'psswd' => '$2y$10$.4O1NR9FSOztr52bLw7aC.inKjVi0uLZ6SSTSYLELuAJmcerF4B7W'),array('email' => 'juan@gmail.com', 'psswd' => '$2y$10$.4O1NR9FSOztr52bLw7aC.inKjVi0uLZ6SSTSYLELuAJmcerF4B7W'),array('email' => 'maria@yahoo.es', 'psswd' => '$2y$10$.4O1NR9FSOztr52bLw7aC.inKjVi0uLZ6SSTSYLELuAJmcerF4B7W'));
 
 $app = new \Slim\App;
 
@@ -20,29 +20,49 @@ $app->get('/', function (Request $request, Response $response) {
     return $response;
 });
 
-$app->post('/login', function (Request $request, Response $response) use ($users){
+$app->post('/login', function (Request $request, Response $response) use ($mysqli){
     $json = $request->getBody();
     $login = json_decode($json);
-    $userOk = array_filter($users, function ($item) use ($login) {
-        return ($item['email'] === $login->email && password_verify($login->psswd, $item['psswd'])); //$item['psswd'] === sha1($login->psswd));
-    });
-    //$response->getBody()->write($json);
-//    $response = $response->withJson($userOk);
-    if (sizeof($userOk) > 0){
+
+    $stmt = $mysqli->prepare("SELECT password FROm users WHERE email = ?");
+    $stmt->bind_param("s", $login->email);
+    $userOk = $stmt->execute();
+    $stmt->bind_result($password);
+    $stmt->fetch();
+
+    $stmt->close();
+    $mysqli->close();
+
+    if (!is_null($password)){
         var_dump(hash("sha256",'$5$rounds=5000$anexamplestringforsalt$'));
-        $token = crypt($login->email, hash("sha256",'$5$rounds=5000$anexamplestringforsalt$'));
-        $response->getBody()->write($token);
+        if (password_verify($login->psswd, $password)){
+            $token = crypt($login->email, hash("sha256",'$5$rounds=5000$anexamplestringforsalt$'));
+            $response->getBody()->write($token);
+        } else {
+            $response->getBody()->write("incorrect password");
+        }
+
     } else {
-        $response->getBody()->write("fail");
+        $response->getBody()->write("user not found");
     }
     return $response;
 });
 
-$app->post('/register', function (Request $request, Response $response){
+$app->post('/register', function (Request $request, Response $response) use ($mysqli){
     $json = $request->getBody();
     $login = json_decode($json);
     $hashed_password = password_hash($login->psswd, PASSWORD_DEFAULT);
-    $response->getBody()->write($hashed_password);
+
+    $stmt = $mysqli->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $login->email, $hashed_password);    
+    if ($stmt->execute()===TRUE){
+            $response->getBody()->write($hashed_password);
+    } else {
+            $response->getBody()->write("error");
+    }    
+    var_dump($hashed_password);
+    $stmt->close();
+    $mysqli->close();
     return $response;
 });
 
